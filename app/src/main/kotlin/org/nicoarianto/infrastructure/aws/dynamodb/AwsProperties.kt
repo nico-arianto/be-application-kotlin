a229b1050390eedb28d15dc9134b7e1b01f4ae21
Nico Arianto <nico.arianto@live.com.sg>
2022-11-25 22:02:15 +0800

    Upgrade the AWS SDK to 2.18.24, Spring Boot to 3.0.0 and Spring Cloud to 2021.0.5

--- app/src/main/kotlin/org/nicoarianto/infrastructure/aws/dynamodb/AwsProperties.kt
+++ app/src/main/kotlin/org/nicoarianto/infrastructure/aws/dynamodb/AwsProperties.kt
@@ -1,9 +1,7 @@
 package org.nicoarianto.infrastructure.aws.dynamodb
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.ConstructorBinding
 import java.net.URI
 
 @ConfigurationProperties("aws")
-@ConstructorBinding
 data class AwsProperties(val accessKeyId: String?, val secretAccessKey: String?, val region: String, val endpoint: URI?)
