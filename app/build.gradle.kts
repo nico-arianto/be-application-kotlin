d26d1c46e523b44435040c1fa80391a9b5d4f658
Nico Arianto <nico.arianto@live.com.sg>
2022-12-12 20:48:58 +0800

    Upgrade the AWS SDK to 2.18.35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ repositories {
 dependencies {
     // Align versions of all Kotlin components
     implementation(platform(kotlin("bom")))
-    implementation(platform("software.amazon.awssdk:bom:2.18.34"))
+    implementation(platform("software.amazon.awssdk:bom:2.18.35"))
 
     // Use the Kotlin JDK 8 standard library.
     implementation(kotlin("stdlib-jdk8"))
