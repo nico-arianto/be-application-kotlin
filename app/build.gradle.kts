d4ccd13e55c582d3e33763dec06ea2b709a62a90
Nico Arianto <nico.arianto@live.com.sg>
2022-12-21 22:13:15 +0800

    Upgrade the AWS ASK to 2.19.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ repositories {
 dependencies {
     // Align versions of all Kotlin components
     implementation(platform(kotlin("bom")))
-    implementation(platform("software.amazon.awssdk:bom:2.18.41"))
+    implementation(platform("software.amazon.awssdk:bom:2.19.1"))
 
     // Use the Kotlin JDK 8 standard library.
     implementation(kotlin("stdlib-jdk8"))
