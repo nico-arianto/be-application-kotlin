ca0f893328116a82c43142f7ca89922e90bee096
Nico Arianto <nico.arianto@live.com.sg>
2023-01-04 22:26:21 +0800

    Upgrade the AWS SDK to 2.19.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ repositories {
 dependencies {
     // Align versions of all Kotlin components
     implementation(platform(kotlin("bom")))
-    implementation(platform("software.amazon.awssdk:bom:2.19.8"))
+    implementation(platform("software.amazon.awssdk:bom:2.19.9"))
 
     // Use the Kotlin JDK 8 standard library.
     implementation(kotlin("stdlib-jdk8"))
